70b485eab2c0b5c28e157e72026b390999ce6cca
Gabriel <gabrieldv.dv@hotmail.com>
2019-10-20 00:12:11 -0300

    New components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { CadastrarComponent } from './menu/cadastrar/cadastrar.component';
+import { ListarComponent } from './menu/listar/listar.component';
+import { DeletarComponent } from './menu/deletar/deletar.component';
+import { AtualizarComponent } from './menu/atualizar/atualizar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent }, 
+  { path: 'cadastrar', component:CadastrarComponent },
+  { path: 'lista', component: ListarComponent },
+  { path: 'apagar', component: DeletarComponent },
+  { path: 'atualizar', component: AtualizarComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CadastrarComponent } from './menu/cadastrar/cadastrar.component';
+import { ListarComponent } from './menu/listar/listar.component';
+import { DeletarComponent } from './menu/deletar/deletar.component';
+import { AtualizarComponent } from './menu/atualizar/atualizar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastrarComponent,
+    ListarComponent,
+    DeletarComponent,
+    AtualizarComponent
   ],
   imports: [
     BrowserModule,
