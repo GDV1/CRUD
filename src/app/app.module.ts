8f6e70308cd2282b149f47775b2a6208e1dc1278
Gabriel <gabrieldv.dv@hotmail.com>
2019-11-17 00:00:18 -0300

    Removing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { CadastrarComponent } from './menu/cadastrar/cadastrar.component';
-import { ListarComponent } from './menu/listar/listar.component';
-import { DeletarComponent } from './menu/deletar/deletar.component';
-import { AtualizarComponent } from './menu/atualizar/atualizar.component';
 import { FormsModule } from '@angular/forms';
 
-// Configuração Firebase
+// Firebase settings - with AngularFire
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireFunctionsModule } from '@angular/fire/functions';
@@ -19,12 +14,7 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent,
-    CadastrarComponent,
-    ListarComponent,
-    DeletarComponent,
-    AtualizarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
