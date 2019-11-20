c34878ac5e214755fd3a77900e4dbdcc031b6996
Gabriel <gabrieldv.dv@hotmail.com>
2019-11-20 00:32:37 -0300

    Change environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC5hZMJGMMlQvJ6QYxsD5gxdCiXv3JTDHo",
+    authDomain: "receita-facil-c4846.firebaseapp.com",
+    databaseURL: "https://receita-facil-c4846.firebaseio.com",
+    projectId: "receita-facil-c4846",
+    storageBucket: "receita-facil-c4846.appspot.com",
+    messagingSenderId: "802749554916",
+    appId: "1:802749554916:web:04b31a894a2c0400c98cf1",
+    measurementId: "G-8QDJZW1Q24"
+  },
 };
